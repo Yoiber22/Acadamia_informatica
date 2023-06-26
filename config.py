ff1a5adf46d74ad6714588f6a6dcd99778e67d3c
Yoiber <yoibermendoza22@gmail.com>
2023-06-26 01:29:04 -0300

    Actualizando config

--- config.py
+++ config.py
@@ -6,11 +6,10 @@ from datetime import datetime
 nom_cursos      = 'data/cursos.txt'
 nom_alumnos     = 'data/alumnos.txt'
 
-nom_pacientes   = 'data/pacientes.txt'
 
 FORMATO_FECHA   = '%d/%m/%Y'
 FORMATO_HORA    = '&H:%M'
-FORMATO_HORA_12 = '%I:%M %p'    # Formato 12 hs  
+FORMATO_HORA_12 = '%I:%M %p'    
 FORMATO_BD      = '%Y-%m-%d'
 
 
