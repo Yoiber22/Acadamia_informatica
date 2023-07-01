922d1fff1355a4ebe7ac3330512316fefc8fd78c
Yoiber <yoibermendoza22@gmail.com>
2023-06-30 23:57:30 -0300

    Actualizando cursos

--- cursos.py
+++ cursos.py
@@ -1,71 +1,88 @@
 from funciones import *
 
+if existe_archivo(direc_cursos):
+    registro_cursos = registros(direc_cursos)
+
+def verificar_nombre_curso(nombre):
+    archivo = open(direc_cursos,'r')
+    cursos = archivo.readlines()
+    archivo.close()
+    for curso in cursos:
+        curso = curso.split(',')
+        nombre_analizar = curso[1].rstrip('\n')
+        if nombre_analizar == nombre:
+            return True
+    
+    return False
+
 def agregar_cursos():
     # Agrega cursos a un archivo .txt
     
     while True:
-        if existe_archivo(nom_cursos):
-            id_curso = ultima_clave(nom_cursos) + 1
+        if existe_archivo(direc_cursos):
+            id_curso = ultima_clave(direc_cursos) + 1
             clear()
-            print('Agregar curso\n')
+            titulo('Agregar curso')
             print('Nro curso: ',id_curso)
             while True:
-                nombre_curso = input('Ingrese nombre: ').title()
-                if len(nombre_curso) > 0:
+                nombre_curso = input(COLOR_INPUT+'Ingrese nombre: ').title()
+                if len(nombre_curso) > 0 and verificar_nombre_curso(nombre_curso):
                     break
                 else:
-                    input('Campo obligatorio.')
+                    print(COLOR_ERROR+'Campo obligatorio.')
 
-            if input('¿Desea agregar este curso? (s/n): ').lower() =='s':
-                archivo = open(nom_cursos,'a',encoding='utf-8')
+            if input(COLOR_INPUT+'¿Desea agregar este curso? (s/n): ').lower() =='s':
+                archivo = open(direc_cursos,'a',encoding='utf-8')
                 registro = f'{id_curso},{nombre_curso}\n'
                 archivo.write(registro)
                 archivo.close()
-                print('\nCurso agregado existosamente\n')
+                print(COLOR_DESTACAR+'\nCurso agregado existosamente\n')
             
             else:
-                print('\nAgregado canselado\n')
+                print(COLOR_DESTACAR+'\nAgregado canselado\n')
 
-            if input('¿Desea agregar otro curso? (s/n): ').lower() !='s':
+            if input(COLOR_INPUT+'¿Desea agregar otro curso? (s/n): ').lower() !='s':
                 break
         else:
-            archivo = open(nom_cursos,'w')
+            archivo = open(direc_cursos,'w')
             archivo.close()
             agregar_cursos()
 
 def eliminar_cursos():
     # Elimina un curso que no esta siedo cursado 
     # por ningun alumno
-    while True:
-        try:
-            clear()
-            print('Elimanar curso\n')
-            id_borrar = int(input('Ingrese ID de curso a borrar: '))
-            if not alumnos_curso(nom_alumnos,id_borrar):
-                nombre_curso = data_registro(nom_cursos,id_borrar)[1]
-                print('Curso a borrar: ',nombre_curso)
+    if existe_archivo(direc_cursos) and len(registro_cursos) > 0:
+        while True:
+            try:
+                clear()
+                titulo('Elimanar curso')
+                id_borrar = int(input(COLOR_INPUT+'Ingrese ID de curso a borrar: '))
+                if not alumnos_curso(direc_alumnos,id_borrar):
+                    nombre_curso = data_registro(direc_cursos,id_borrar)[1]
+                    print('Curso a borrar: ',nombre_curso)
 
-                if input('¿Seguro desea borrar este curso? (s/n): ').lower() =='s':
-                    borrar_registros(nom_cursos,id_borrar)
-                    print('\nCurso borrado exitosmente\n')
+                    if input(COLOR_INPUT+'¿Seguro desea borrar este curso? (s/n): ').lower() =='s':
+                        borrar_registros(direc_cursos,id_borrar)
+                        print(COLOR_DESTACAR+'\nCurso borrado exitosmente\n')
+                    else:
+                        input(COLOR_DESTACAR+'Borrrado cancelado.')
                 else:
-                    input('Borrrado cancelado.')
+                    input(COLOR_ERROR+'Hay alumnos en este curso.')
 
-            else:
-                input('Hay alumnos en este curso.')
+                if input(COLOR_INPUT+'¿Desea eliminar otro curso? (s/n): ').lower() !='s':
+                    break
 
-            if input('¿Desea eliminar otro curso? (s/n): ').lower() !='s':
-                break
-
-        except ValueError:
-            input('ID invalida.')
-        
+            except ValueError:
+                input(COLOR_ERROR+'ID invalida.')
+    else:
+        input(COLOR_ERROR+'No hay cursos para eliminar.')
+        menu_cursos()
 
 def listar_cursos():
     # Muestra los cursos del archivo .txt
 
-    if existe_archivo(nom_cursos):
-        archivo = open(nom_cursos,'r')
+    if existe_archivo(direc_cursos):
+        archivo = open(direc_cursos,'r')
         registros = archivo.readlines()
         archivo.close()
         lista_registros = []
@@ -75,16 +92,16 @@ def listar_cursos():
 
         if len(registros) > 0:
             clear()
-            print('Listado de cursos\n')
+            titulo('Listado de cursos')
             encabezado = ['Id','+Cursos']
             print(tabulate(lista_registros,headers=encabezado,tablefmt='rounded_outline',numalign='center'))
             print()
-            input('Puse enter para continuar...')
-
+            pausa()
+            
         else:
-            ('No se encontraron cursos.')
+            input(COLOR_ERROR+'No se encontraron cursos.')
     else:
-        ('No existe el archivo.')
+        input(COLOR_ERROR+'No existe el archivo.')
 
 def alumnos_curso(nombre_archivo,id_curso):
     # Me dice si algun alumno esta cursando el curso ingresado
@@ -97,18 +114,17 @@ def alumnos_curso(nombre_archivo,id_curso):
         if int(alumno[5]) == id_curso:
             return True
     return False
-    
 
 def menu_cursos():
     while True:
         try:
             clear()
-            print('Sistema de Cursos\n')
+            titulo('Sistema de Cursos')
             print('1. Agregar cursos')
             print('2. Eliminar cursos')
             print('3. Listar cursos')
             print('0. Volver al menu principal\n')
-            opcion = int(input('Elija opcion: '))
+            opcion = int(input(COLOR_INPUT+'Elija opcion: '))
             if opcion in range(0,4):
 
                 if   opcion == 0: break
@@ -117,6 +133,6 @@ def menu_cursos():
                 elif opcion == 3: listar_cursos()
             
             else:
-                input('Opcion no encontrda.')
+                print(COLOR_ERROR+'Opcion no encontrda.')
         except ValueError:
-            input('Opcion invalida.')
+            input(COLOR_ERROR+'Opcion invalida.')
